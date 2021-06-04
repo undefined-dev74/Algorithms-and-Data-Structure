f8b18f9d06111d6a5681ed6c27449722056b53cb
JuniorBaymax <cooladitya953@gmail.com>
2021-06-04 22:57:30 +0530

    Completed Bubble sorting Algorithm

---
+++ Sorting Algorithms/BubbleSort.js
@@ -0,0 +1,14 @@
+const BubbleSort = arr => {
+  for (let i = arr.length; i > 0; i--) {
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        const temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+};
+
+console.log(BubbleSort([13, 3, 21, 12, 88, 9]));
