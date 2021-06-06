5d5d6006b2b533d7da3e9ea968d2db17fcf19e14
JuniorBaymax <cooladitya953@gmail.com>
2021-06-06 23:18:53 +0530

    Code Optimization: Added noSwaps to improve code efficiancy

--- Sorting Algorithms/BubbleSort.js
+++ Sorting Algorithms/BubbleSort.js
@@ -1,12 +1,15 @@
 const BubbleSort = arr => {
+  let noSwaps = true; // adding noswaps to optimize the code efficiancy
   for (let i = arr.length; i > 0; i--) {
     for (let j = 0; j < i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
         const temp = arr[j];
         arr[j] = arr[j + 1];
         arr[j + 1] = temp;
+        noSwaps = false;
       }
     }
+    if (noSwaps) break;
   }
   return arr;
 };
